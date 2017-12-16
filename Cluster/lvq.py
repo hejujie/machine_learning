07864750f1fb7aaf03ba691ef37ede865a60cbc6
hejujie <hejujie8@gmail.com>
2017-12-16 12:19:18 +0800

    add comment to lvq

--- Cluster/lvq.py
+++ Cluster/lvq.py
@@ -15,15 +15,31 @@ def read_data(filename = 'data/lvq_test.csv'):
     return np.array(df1)
     
 def lvq(X, Y, q, max_iter = 100, eta = 0.1):
+    """
+    Input:  
+        X: dataset
+        Y: label
+        q: number of vector
+        max_iter: the iteration number
+        eta: the learning rate
+    Output: 
+        X_vec: the vector after max_iteration
+    """
+    
+    # Initial Vector
     n = X.shape[0]
     sample = random.sample(range(n), q)
     X_vec = X[sample]
     Y_vec = Y[sample]
 
+    # Update Vector base on the label of random_choice data and vector
+    # if label is the same, the closest vector_point will be colser to data, otherwise: farther
     for iters in range(max_iter):
         index = random.randint(0, n-1)
+        
         distance = np.sqrt(np.sum(np.square(X_vec - X[index]), axis = 1))
         min_q = np.argmin(distance, axis = 0)
+        
         if Y_vec[min_q] == Y[index]:
             X_vec[min_q] = X_vec[min_q] + eta * (X[index] - X_vec[min_q])
         else:
@@ -32,8 +48,17 @@ def lvq(X, Y, q, max_iter = 100, eta = 0.1):
     return X_vec
     
 def divide(X, X_vec):
+    """
+    Input:
+        X: the primer data
+        X_vec: the value of vector
+    Output:
+        divide_index: the index of data base on the closest distance to vector
+    """
     n = X.shape[0]
     q = X_vec.shape[0]
+    
+    # distance is an [q, n] matrix, means the distance between every data point to vector point
     distance = np.zeros((q, n))
     for i in range(q):
         distance[i] = np.sqrt(np.sum(np.square(X - X_vec[i]), axis = 1))
